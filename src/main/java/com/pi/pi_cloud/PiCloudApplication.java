0d98d6595c7229324c467d596c2f16c3fdf54ba9
Franc Tauste González <franctauste2000@gmail.com>
2025-10-14 16:36:37 +0200

    Ficheros iniciales

---
+++ src/main/java/com/pi/pi_cloud/PiCloudApplication.java
@@ -0,0 +1,13 @@
+package com.pi.pi_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiCloudApplication.class, args);
+	}
+
+}
